6745d58ebc4347a225530abf953f7458b0e1a41e
kse-music <dh.hiekn@gmail.com>
2018-03-16 13:58:56 +0800

    jwt

--- src/main/java/com/hiekn/metaboot/util/JsonUtils.java
+++ src/main/java/com/hiekn/metaboot/util/JsonUtils.java
@@ -1,13 +1,11 @@
 package com.hiekn.metaboot.util;
 
 import com.google.gson.Gson;
-import com.hiekn.metaboot.exception.ErrorCodes;
 import com.hiekn.metaboot.exception.JsonException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 
 import java.lang.reflect.Type;
-import java.util.Objects;
 
 @Configuration
 public class JsonUtils {
@@ -20,9 +18,6 @@ public class JsonUtils {
     }
 
     public static <T> T fromJson(String json, Class<T> cls) {
-        if(Objects.isNull(json)){
-            throw JsonException.newInstance(ErrorCodes.PARAM_PARSE_ERROR);
-        }
 		try {
 			return gson.fromJson(json, cls);
 		} catch (Exception e) {
@@ -31,9 +26,6 @@ public class JsonUtils {
 	}
 
 	public static <T> T fromJson(String json, Type typeOfT) {
-        if(Objects.isNull(json)){
-            throw JsonException.newInstance(ErrorCodes.PARAM_PARSE_ERROR);
-        }
 		try {
 			return gson.fromJson(json, typeOfT);
 		} catch (Exception e) {
