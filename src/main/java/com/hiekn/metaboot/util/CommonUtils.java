b58c6020bbbdcaef5bc1911700317b8881f9e5f6
DingHao <dh.hiekn@gmail.com>
2018-04-22 22:00:24 +0800

    全面使用mcn-spring-boot-starter 2.6.5

--- src/main/java/com/hiekn/metaboot/util/CommonUtils.java
+++ src/main/java/com/hiekn/metaboot/util/CommonUtils.java
@@ -1,10 +1,6 @@
 package com.hiekn.metaboot.util;
 
-import java.io.IOException;
-import java.io.InputStream;
 import java.util.Date;
-import java.util.Objects;
-import java.util.Properties;
 import java.util.UUID;
 
 public final class CommonUtils {
@@ -17,16 +13,4 @@ public final class CommonUtils {
 		return new Date();
 	}
 
-    public static Properties getProperties(Class<?> clazz, String fileName) {
-        InputStream in;
-        Properties props = new Properties();
-        try {
-            in = clazz.getClassLoader().getResourceAsStream(fileName);
-            props.load(in);
-        } catch (IOException e) {
-
-        }
-
-        return props;
-    }
 }
