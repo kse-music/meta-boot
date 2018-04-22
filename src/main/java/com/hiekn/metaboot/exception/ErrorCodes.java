147549062e4a026713d1b9515d2704c649c616bc
DingHao <dh.hiekn@gmail.com>
2018-04-22 16:10:11 +0800

    全面使用mcn-spring-boot-starter 2.6.3

--- src/main/java/com/hiekn/metaboot/exception/ErrorCodes.java
+++ src/main/java/com/hiekn/metaboot/exception/ErrorCodes.java
@@ -1,13 +1,14 @@
 package com.hiekn.metaboot.exception;
 
 import com.hiekn.boot.autoconfigure.base.exception.ExceptionKeys;
-
+/*
+    3xxxx:通用错误码定义
+    5xxxx:业务相关错误码定义
+    7xxxx:未知错误码
+    8xxxx:Http相关错误码定义
+    9xxxx:统一错误码及第三方服务错误码定义
+ */
 public interface ErrorCodes extends ExceptionKeys {
-    //3xxxx:通用错误码定义
-    //5xxxx:业务相关错误码定义
-    //7xxxx:未知错误码
-    //8xxxx:Http相关错误码定义
-    //9xxxx:统一错误码及第三方服务错误码定义
 
-    Integer AUTHENTICATION_ERROR = 50012;
+    Integer INIT_INTENT_ERROR = 50003;
 }
