d9db3b04618495bec036badfd5b58462b557bc0b
kse-music <dh.hiekn@gmail.com>
2018-05-23 16:13:03 +0800

    use mcn 2.7.6

--- src/main/java/com/hiekn/metaboot/MetaBootApplication.java
+++ src/main/java/com/hiekn/metaboot/MetaBootApplication.java
@@ -1,11 +1,9 @@
 package com.hiekn.metaboot;
 
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@MapperScan("com.hiekn.metaboot.dao")
 public class MetaBootApplication {
 
 	public static void main(String[] args) {
