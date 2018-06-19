a4e2eec352c9e7e4446f503fc0c91748e72a0ab2
kse-music <dh.hiekn@gmail.com>
2018-06-19 18:16:12 +0800

    将常量和配置管理分开

--- src/main/java/com/hiekn/metaboot/conf/ConfigManage.java
+++ src/main/java/com/hiekn/metaboot/conf/ConfigManage.java
@@ -1,5 +1,6 @@
 package com.hiekn.metaboot.conf;
 
+
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
@@ -7,8 +8,15 @@ import org.springframework.context.annotation.PropertySource;
 @Configuration
 @PropertySource("${custom.property.path}")
 @ConfigurationProperties
-public class Constants {
+public class ConfigManage {
 
-    public static final String VAR = "常量";
+    private String sseLocation;
 
+    public String getSseLocation() {
+        return sseLocation;
+    }
+
+    public void setSseLocation(String sseLocation) {
+        this.sseLocation = sseLocation;
+    }
 }
