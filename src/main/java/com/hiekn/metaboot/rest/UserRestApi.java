4b1181566be3ac61eae8e4fb236e300dd7184825
kse-music <dh.hiekn@gmail.com>
2018-07-17 12:33:22 +0800

    规范话restful路径

--- src/main/java/com/hiekn/metaboot/rest/UserRestApi.java
+++ src/main/java/com/hiekn/metaboot/rest/UserRestApi.java
@@ -10,13 +10,14 @@ import com.hiekn.metaboot.service.UserService;
 import io.swagger.annotations.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import javax.validation.Valid;
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 
 @Controller
-@Path("/user")
+@Path("user")
 @Produces(MediaType.APPLICATION_JSON)
 @Api("用户模块")
 @ApiImplicitParams({@ApiImplicitParam(paramType = "header", dataType = "string", name = "Authorization",required = true)})
@@ -26,7 +27,7 @@ public class UserRestApi {
     private UserService userService;
 
     @GET
-    @Path("/list/page")
+    @Path("list/page")
     @ApiOperation("分页")
     public RestResp<RestData<UserBean>> listByPage(@Valid @BeanParam PageModel pageModel,
                                                    @QueryParam("mobile")String mobile) {
@@ -38,16 +39,16 @@ public class UserRestApi {
     }
 
     @GET
-    @Path("/get")
+    @Path("{id}")
     @ApiOperation("获取")
-    public RestResp<UserBean> get(@ApiParam(required = true)@QueryParam("id") String id) {
+    public RestResp<UserBean> get(@PathParam("id") String id) {
         return new RestResp<>(userService.getByPrimaryKey(id));
     }
 
-    @GET
-    @Path("/delete")
+    @DELETE
+    @Path("{id}/remove")
     @ApiOperation("删除")
-    public RestResp delete(@ApiParam(required = true)@QueryParam("id") String id) {
+    public RestResp delete(@PathParam("id") String id) {
         userService.deleteByPrimaryKey(id);
         return new RestResp<>();
     }
@@ -62,10 +63,10 @@ public class UserRestApi {
         return new RestResp<>(userBean);
     }
 
-    @POST
-    @Path("/update")
+    @PUT
+    @Path("{id}/update")
     @ApiOperation("修改")
-    public RestResp update(@ApiParam(required = true)@QueryParam("id") String id,
+    public RestResp update(@PathParam("id") String id,
                            @ApiParam(required = true)@FormParam("bean") String bean) {
         UserBean userBean = JsonUtils.fromJson(bean, UserBean.class);
         userBean.setId(id);
@@ -74,15 +75,15 @@ public class UserRestApi {
     }
 
     @POST
-    @Path("/login")
+    @Path("login")
     @ApiOperation("登录")
     public RestResp<UserBean> login(@ApiParam(value="手机号",required=true)@FormParam("mobile") String mobile,
                                     @ApiParam(value="密码",required=true)@FormParam("password") String password){
         return new RestResp<>(userService.login(mobile,password));
     }
 
-    @GET
-    @Path("/logout")
+    @POST
+    @Path("logout")
     @ApiOperation("登出")
     public RestResp<Object> logout(){
         userService.logout();
