28c5c3c6e5a2e11913c79b32e4f66e09a07c1da4
kse-music <dh.hiekn@gmail.com>
2019-03-04 15:42:55 +0800

    remove custom properties

--- src/main/java/com/hiekn/metaboot/conf/ConfigManage.java
+++ src/main/java/com/hiekn/metaboot/conf/ConfigManage.java
@@ -1,12 +1,9 @@
 package com.hiekn.metaboot.conf;
 
-
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
 
 @Configuration
-@PropertySource("${custom.property.path}")
 @ConfigurationProperties
 public class ConfigManage {
 
