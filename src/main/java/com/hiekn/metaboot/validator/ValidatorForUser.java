bd68ec757db8d8fd898730ff1af628e88e254f3a
DingHao <dh.hiekn@gmail.com>
2022-01-28 14:15:54 +0800

    use ConstraintValidator

--- src/main/java/com/hiekn/metaboot/validator/ValidatorForUser.java
+++ src/main/java/com/hiekn/metaboot/validator/ValidatorForUser.java
@@ -1,22 +1,22 @@
 package com.hiekn.metaboot.validator;
 
-import cn.hiboot.mcn.autoconfigure.validator.ConstraintValidatorExtend;
 import com.hiekn.metaboot.bean.param.UserParam;
 import com.hiekn.metaboot.bean.po.UserBean;
 import com.hiekn.metaboot.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 import java.util.Objects;
 
-public class ValidatorForUser implements ConstraintValidatorExtend<UserValidator, UserParam> {
+public class ValidatorForUser implements ConstraintValidator<UserValidator, UserParam> {
 
     @Autowired
     private UserService userService;
 
-    private void config(ConstraintValidatorContext context,String msg) {
+    private void config(ConstraintValidatorContext context) {
         context.disableDefaultConstraintViolation();
-        context.buildConstraintViolationWithTemplate(msg).addConstraintViolation();
+        context.buildConstraintViolationWithTemplate("手机号已存在").addConstraintViolation();
     }
 
     @Override
@@ -28,7 +28,7 @@ public class ValidatorForUser implements ConstraintValidatorExtend<UserValidator
         if(Objects.isNull(bean)){
             return true;
         }
-        config(context,"手机号已存在");
+        config(context);
         return false;
     }
 
